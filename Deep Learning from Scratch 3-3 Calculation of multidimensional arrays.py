84e653f9f0c95d1deebe5e6bc10ba6e687202e56
jsjj10002 <88393724+jsjj10002@users.noreply.github.com>
2023-07-13 22:37:16 +0900

    Update and rename 3-3 Calculation of multidimensional arrays.py to Deep Learning from Scratch 3-3 Calculation of multidimensional arrays.py
    다차원 배열의 계산(행렬의 곱, 행렬곱을 활용한 신경망 표현)

--- Deep Learning from Scratch 3-3 Calculation of multidimensional arrays.py
+++ Deep Learning from Scratch 3-3 Calculation of multidimensional arrays.py
@@ -1,166 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#다차원 배열
 import numpy as np
-
-
-# In[2]:
-
-
 A = np.array([1,2,3,4])
 print(A)
-
-
-# In[3]:
-
-
+#[1 2 3 4]
 #Dimensions of the array
 np.ndim(A)
-
-
-# In[4]:
-
-
-A.shape #Return a tuple
-
-
-# In[5]:
-
-
+# 1 
+A.shape #Return a tuple/ 배열의 형상 확인 
+# (4, )
 A.shape[0]
+# 4
 
-
-# In[6]:
-
-
+# 2차원 배열 
 B = np.array([[1,2],[3,4],[5,6]])
 print(B)
-
-
-# In[7]:
-
-
 np.ndim(B)
-
-
-# In[8]:
-
-
+2
 B.shape
+(3,2)
 
-
-# In[9]:
-
-
+# 행렬의 곱 
 A=np.array([[1,2],[3,4]])
 A.shape
-
-
-# In[10]:
-
-
+# (2,2)
 B=np.array([[5,6],[7,8]])
 B.shape
-
-
-# In[11]:
-
-
-np.dot(A,B)
-
-
-# In[15]:
-
-
-np.dot(B,A)
-
-
-# In[13]:
-
+# (2,2)
+np.dot(A,B) #입력이 1차원 배열이면 벡터를, 2차원 배열이면 행렬 곱을 계산 
+np.dot(B,A) #주의// np.dot(A,B) != np.dot(B,A) 
 
 A=np.array([[1,2,3],[4,5,6]])
 A.shape
-
-
-# In[14]:
-
-
+# (2,3)
 B=np.array([[1,2],[3,4],[5,6]])
 B.shape
-
-
-# In[16]:
-
-
+# (3,2)
 np.dot(A,B)
-
-
-# In[17]:
-
 
 C=np.array([[1,2],[3,4]])
 C.shape
-
-
-# In[18]:
-
-
+# (2,2)
 np.dot(A,C)
-
-
-# In[19]:
-
-
+# 오류 출력 
 np.dot(C,A)
-
-
-# In[20]:
-
 
 A=np.array([[1,2],[3,4],[5,6]])
 A.shape
-
-
-# In[25]:
-
-
+# (3,2)
 B=np.array([7,8])
 B.shape
-
-
-# In[26]:
-
-
+# (2, )
 np.dot(A,B)
-
-
-# In[27]:
-
 
 #scala product in neural network
 X=np.array([1,2])
 X.shape
-
-
-# In[28]:
-
-
 W=np.array([[1,3,5],[2,4,6]])
 print(W)
-
-
-# In[29]:
-
-
 W.shape
-
-
-# In[30]:
-
-
 Y=np.dot(X,W)
 print(Y)
-
