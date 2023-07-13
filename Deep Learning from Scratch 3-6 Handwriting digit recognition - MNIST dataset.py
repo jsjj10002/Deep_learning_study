b25cd8c12514abd6fffa99bcd7f9655c00d80e2f
jsjj10002 <88393724+jsjj10002@users.noreply.github.com>
2023-07-13 23:55:03 +0900

    MNIST 데이터셋 분류 - 순전파 (일반, 배치)
    학습 과정없이 주어진 매게변수로 추론 과정만

--- Deep Learning from Scratch 3-6 Handwriting digit recognition - MNIST dataset.py
+++ Deep Learning from Scratch 3-6 Handwriting digit recognition - MNIST dataset.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
+#MNIST 데이터셋 
+#MNIST 데이터셋: 0~9까지의 숫자 이미지로 구성 훈련 이미지 60000장, 시험 이미지 10000장 준비됨 
+# 28*28크기의 회색조 이미지 , 각 픽셀: 0~255까지의 값을 취함 , 각 이미지에 실제 의미하는 숫자ㅏ 레이블로 붙어있음 
 
 #부모 디렉터리의 파일을 가져올 수 있도록 설정
 import sys , os
@@ -10,18 +8,12 @@ sys.path.append(os.pardir)
 #.dataset/mnist.py의 load_mnist를 임포트 
 from dataset.mnist import load_mnist
 
-
-# In[7]:
-
-
-#(훈련이미지,훈련레이블), (시험 이미지, 시험 레이블) 형태로 반환
+#데이터를 (훈련이미지,훈련레이블), (시험 이미지, 시험 레이블) 형태로 반환
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 #인수로 nomalize, flatten, one_hot_label 설정가능 (bool)
+#nomalize: 픽셀 값  정규화 여부 flatten: 입력 이미지 평탄화(1차원 배열화)여부 one_hot_label: 원-핫 인코딩 여부 
 
-
-# In[8]:
-
-
+# 각 데이터의 형상 출력 
 print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
@@ -29,9 +21,6 @@ print(t_test.shape)
 
 
 # ### MNIST 데이터를 화면에 불러오기
-
-# In[11]:
-
 
 import sys, os
 sys.path.append(os.pardir)
@@ -58,14 +47,7 @@ img_show(img)
 
 # ### 신경망 구축
 
-# In[16]:
-
-
 import pickle
-
-
-# In[19]:
-
 
 #활성함수 
 def sigmoid(x):
@@ -75,14 +57,12 @@ def softmax(a):
     c = np.max(a)
     return np.exp(a-c)/np.sum(np.exp(a-c))
 
-
-# In[23]:
-
+#입력층: 28*28=784개 출력층: 10개
 
 def get_data():
     (x_train, t_train), (x_test, t_test) =     load_mnist(flatten=True, normalize=True, one_hot_label=False)
     #nomalize=True 각 픽셀 값을 0.0~1.0으로 정규화(전처리) 
-    #flatten=True 2차원을 1차월으로 가지고옴 28*28->784
+    #flatten=True 2차원을 1차원으로 가지고옴 28*28->784
     return x_test, t_test
 
 #pickle파일에 저장되어 있는 '학습된 가중치 매개변수'를 읽음-가중치 편향 매개변수 딕셔너리로 저장
@@ -106,10 +86,6 @@ def predict(network, x):
     return y
     
 
-
-# In[26]:
-
-
 #정확도 계산 
 x, t = get_data() #mnist 데이터 얻음 
 network = init_network() #네트워크 생성
@@ -123,49 +99,24 @@ for i in range(len(x)): #데이터 하나씩 분류 - 확률을 넘파이 배열
 
 print("Accuracy:" + str(float(accuracy_cnt)/len(x)) ) #정확도 구함 
 
-
-# In[27]:
-
-
+#배치 처리 
 x,_ = get_data()
 network = init_network()
 W1, W2, W3 = network['W1'], network['W2'], network['W3']
 
-
-# In[28]:
-
-
 x.shape
-
-
-# In[29]:
-
-
+#(10000, 784)
 x[0].shape
-
-
-# In[30]:
-
-
+#(784, )
 W1.shape
-
-
-# In[31]:
-
-
+#(784, 50)
 W2.shape
-
-
-# In[32]:
-
-
+#(50, 100)
 W3.shape
-
+#(100,10)
 
 # ### 배치처리 구현
-
-# In[34]:
-
+#배치: 하나로 묶은 입력 데이터 - 큰 배열을 한꺼번에 계산하는 것이 분할된 작은 배열을 여러 번 계산하는 것 보다 빠름 
 
 x, t = get_data()
 network = init_network()
@@ -180,9 +131,6 @@ for i in range(0, len(x), batch_size):
     accuracy_cnt += np.sum(p==t[i:i+batch_size]) # True가 몇 개인지 셈
     
 print("Accuracy:" + str(float(accuracy_cnt)/len(x)) )
-
-
-# In[39]:
 
 
 #axis예시
