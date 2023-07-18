49b9d1f61856213538ed9fe054d1304b4b8492d3
jsjj10002 <88393724+jsjj10002@users.noreply.github.com>
2023-07-18 23:13:03 +0900

    (수치적) 미분, 편미분 구현 - 중앙차분 개념 이용

--- Deep Learning from Scratch 4-3 Numerrical differentiation.py
+++ Deep Learning from Scratch 4-3 Numerrical differentiation.py
@@ -1,83 +1,47 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
+#수치미분 구현 
 import numpy as np
-
-
-# In[1]:
-
 
 #미분의 나쁜 구현의 예
 def numerical_diff(f,x):
     h = 1e-50 #반올림 오차 (rounding error)문제를 일으킴
     return (f(x+h)-f(x))/h #전방차분
-
-
-# In[4]:
-
-
+    
 np.float32(1e-50)
+# 0.0 32비트로 나타내면 오차가 발생 
 
-
-# In[9]:
-
-
-#개선된 미분 구현 - 1e-4 , 중앙차분
+#두 개선점 적용해 미분 구현 - 1e-4 사용 , 중앙차분: (x+h)와 (x-h)차이의 차분을 계산 
 
 def numerical_diff(f,x):
-    h = 1e-4
+    h = 1e-4 #0.0001
     return (f(x+h)-f(x-h))/(2*h) #중앙 차분
 
-
-# $0.01x^2+0.1x$ 
-
-# In[6]:
-
+#수치미분의 예 
+# $y= 0.01x^2+0.1x$ 함수 정의 
 
 def function_1(x):
     return 0.01*x**2 +0.1*x
-
-
-# In[7]:
-
-
+#함수 그리기 
 import matplotlib.pylab as plt 
 
-x = np.arange(0.0, 20.0, 0.1)
+x = np.arange(0.0, 20.0, 0.1) #0.0에서 20.0까지 0.1간격으로 배열 x를 만듦(20 미포함)
 y = function_1(x)
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.plot(x,y)
 plt.show()
 
+numerical_diff(function_1, 5) # x가 5일때 미분 계산 
+# 0.1999999999990898
+numerical_diff(function_1, 10) # x가 10일때 미분 계산
+# 0.2999999999986347
+#오차가 매우 작음 
 
-# In[10]:
-
-
-numerical_diff(function_1, 5)
-
-
-# In[11]:
-
-
-numerical_diff(function_1, 10)
-
-
-# In[12]:
-
-
+#구한 수치미분 값을 기울기로 하는 직선 그리기 
 def tangent_line(f,x):
     d = numerical_diff(f, x)
     print(d)
     y = f(x) - d*x
     return lambda t: d*t + y
-
-
-# In[13]:
-
 
 x = np.arange(0.0,20.0,0.1)
 y = function_1(x)
@@ -93,29 +57,21 @@ plt.show()
 
 
 # ##### 편미분
-# $ f(x_0, x_1) = x_0^2 + x_1^2 $
+# $ f(x_0, x_1) = x_0^2 + x_1^2 $ 변수가 2개인 함수 
 
-# In[14]:
-
-
+#함수 구현 
 def function_2(x):
     return x[0]**2 + x[1]**2 #인수x : 넘파이 배열
-    # return np.sum(x**2)
-
-
-# In[15]:
+    # 또는 return np.sum(x**2)
 
 
 #x0=3, x1=4 일때 x0에 대한 편미분 
 
+# 1. x1에 4 대입 
 def function_tmp1(x0):
     return x0*x0 + 4.0**2
-
+# 2. x0=3에서 미분 값 구하기 
 numerical_diff(function_tmp1, 3.0)
-
-
-# In[17]:
-
 
 #x0=3, x1=4 일때 x1에 대한 편미분 
 
